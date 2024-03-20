a1181b9516f2fe2bec661f53e0876ee8301eb4ca
xXxPMWxXx <panmingwei123@gmail.com>
2024-03-20 11:20:26 +0800

    init

---
+++ app/src/main/java/com/example/savetherobot/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.savetherobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+    }
+}
