b530eae71b1e3122c0b8cec8d23b041e6592532c
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2024-04-03 21:04:23 +0800

    remove OS status bar

--- app/src/main/java/com/example/savetherobot/MainActivity.java
+++ app/src/main/java/com/example/savetherobot/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.savetherobot;
 
+import android.app.ActionBar;
+import android.os.Build;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,10 +13,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Set the layout to the activity_main.xml file
-        setContentView(R.layout.activity_main);
         // Keep the screen on while the app is running
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Set the layout to the activity_main.xml file
+        setContentView(R.layout.activity_main);
     }
 
     public void startGame(View view) {
