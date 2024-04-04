7f5bc499bd6b75264ee4e07097511f6dee8c6bdf
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2024-04-05 00:06:31 +0800

    updated hiding status bar for all android ver

--- app/src/main/java/com/example/droughtsingapore2100/MainActivity.java
+++ app/src/main/java/com/example/droughtsingapore2100/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.droughtsingapore2100;
 
+import android.app.ActionBar;
+import android.os.Build;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,7 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Keep the screen on while the app is running
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Never show the action bar if the status bar is hidden, so hide that too if necessary.
+            ActionBar actionBar = getActionBar();
+            if(actionBar != null){
+                actionBar.hide();
+            }
+        }
         // Set the layout to the activity_main.xml file
         setContentView(R.layout.activity_main);
     }
